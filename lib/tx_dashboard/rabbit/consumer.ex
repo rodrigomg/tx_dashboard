71a46e7fabe4345399f3501d9b4be6a434dda0ec
technosoul <rockdrigo.mtz@gmail.com>
2022-02-23 21:14:14 -0600

    coding, consume messages queue

--- lib/tx_dashboard/rabbit/consumer.ex
+++ lib/tx_dashboard/rabbit/consumer.ex
@@ -6,8 +6,8 @@ defmodule TxDashboard.Consumer do
     GenServer.start_link(__MODULE__, [], [])
   end
 
-  @exchange "gen_server_test_exchange"
-  @queue "gen_server_test_queue"
+  @exchange "default_exchange"
+  @queue "test_tx"
   @queue_error "#{@queue}_error"
 
   def init(_opts) do
@@ -23,22 +23,23 @@ defmodule TxDashboard.Consumer do
   end
 
   # Confirmation sent by the broker after registering this process as a consumer
-  def handle_info({:basic_consume_ok, %{consumer_tag: consumer_tag}}, chan) do
+  def handle_info({:basic_consume_ok, %{consumer_tag: _consumer_tag}}, chan) do
     {:noreply, chan}
   end
 
   # Sent by the broker when the consumer is unexpectedly cancelled (such as after a queue deletion)
-  def handle_info({:basic_cancel, %{consumer_tag: consumer_tag}}, chan) do
+  def handle_info({:basic_cancel, %{consumer_tag: _consumer_tag}}, chan) do
     {:stop, :normal, chan}
   end
 
   # Confirmation sent by the broker to the consumer process after a Basic.cancel
-  def handle_info({:basic_cancel_ok, %{consumer_tag: consumer_tag}}, chan) do
+  def handle_info({:basic_cancel_ok, %{consumer_tag: _consumer_tag}}, chan) do
     {:noreply, chan}
   end
 
   def handle_info({:basic_deliver, payload, %{delivery_tag: tag, redelivered: redelivered}}, chan) do
     # You might want to run payload consumption in separate Tasks in production
+    IO.inspect("Handle info + payload")
     consume(chan, tag, redelivered, payload)
     {:noreply, chan}
   end
@@ -46,40 +47,11 @@ defmodule TxDashboard.Consumer do
   defp setup_queue(chan) do
     {:ok, _} = Queue.declare(chan, @queue_error, durable: true)
 
-    # Messages that cannot be delivered to any consumer in the main queue will be routed to the error queue
-    {:ok, _} =
-      Queue.declare(chan, @queue,
-        durable: true,
-        arguments: [
-          {"x-dead-letter-exchange", :longstr, ""},
-          {"x-dead-letter-routing-key", :longstr, @queue_error}
-        ]
-      )
-
-    :ok = Exchange.fanout(chan, @exchange, durable: true)
     :ok = Queue.bind(chan, @queue, @exchange)
   end
 
-  defp consume(channel, tag, redelivered, payload) do
-    number = String.to_integer(payload)
-
-    if number <= 10 do
-      :ok = Basic.ack(channel, tag)
-      IO.puts("Consumed a #{number}.")
-    else
-      :ok = Basic.reject(channel, tag, requeue: false)
-      IO.puts("#{number} is too big and was rejected.")
-    end
-  rescue
-    # Requeue unless it's a redelivered message.
-    # This means we will retry consuming a message once in case of exception
-    # before we give up and have it moved to the error queue
-    #
-    # You might also want to catch :exit signal in production code.
-    # Make sure you call ack, nack or reject otherwise consumer will stop
-    # receiving messages.
-    exception ->
-      :ok = Basic.reject(channel, tag, requeue: not redelivered)
-      IO.puts("Error converting #{payload} to integer")
+  defp consume(_channel, _tag, _redelivered, payload) do
+    IO.inspect("Me doblo...")
+    IO.inspect(payload)
   end
 end
